2a0dd180834466e23655f9f044f435595d788379
unknown <pat575859@gmail.com>
2023-06-16 21:59:26 +0200

    fist commit with tailwind css

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Concert from './components/Concert.vue'
+</script>
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Concert />
+</template>
